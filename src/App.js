312638317821a98b1c8c796297a497021ec10298
AndreyKovaleu <Andreykovaleu@gmail.com>
2023-02-25 17:16:58 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+// Контекст
 import React from 'react'
+import ComponentC from './components/ComponentC';
+import { UserProvider } from './components/userContext';
 
 function App() {
   return (
     <div className="App">
+      <UserProvider value='Andrey'>
+        <ComponentC />
+      </UserProvider>
     </div>
   );
 }
