62021e9a9cbb32b88aa652ae8ca101b0281f0fac
AndreyKovaleu <Andreykovaleu@gmail.com>
2023-02-25 16:24:37 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <>
-  //   <App />
-  // </>
   <React.StrictMode>
     <App />
   </React.StrictMode>
